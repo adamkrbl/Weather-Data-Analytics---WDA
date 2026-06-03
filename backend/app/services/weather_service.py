24eae364254f54cec1622171a5026904e3bef2fc
AdamKorb <adam.korbel.16@gmail.com>
2026-06-03 17:03:24 +0200

    update service

--- backend/app/services/weather_service.py
+++ backend/app/services/weather_service.py
@@ -20,21 +20,23 @@ def fetch_weather_data(city_name, latitude, longitude):
 
     response = requests.get(url)
 
-data = response.json()
+    data = response.json()
 
-if "error" in data:
+    if "error" in data:
+        return data
+
+    times = data["hourly"]["time"]
+    temperatures = data["hourly"]["temperature_2m"]
+
+    df = pd.DataFrame({
+        "time": times,
+        "temperature": temperatures
+    })
+
+    file_name = f"data/{city_name.lower()}_weather.csv"
+
+    df.to_csv(file_name, index=False)
+
+    print(f"Weather data saved to {file_name}")
+
     return data
-
-times = data["hourly"]["time"]
-temperatures = data["hourly"]["temperature_2m"]
-
-df = pd.DataFrame({
-    "time": times,
-    "temperature": temperatures
-})
-
-file_name = f"data/{city_name.lower()}_weather.csv"
-
-df.to_csv(file_name, index=False)
-
-return data
